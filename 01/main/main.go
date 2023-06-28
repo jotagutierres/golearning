8672a4832af68a30f2dd0cac06159d62dee8b8eb
Carlos Gutierres <gutierres7j@outlook.com>
2023-06-28 14:13:35 -0300

    teste

---
+++ 01/main/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("😍 test test")
+	fmt.Println(`
+	testando hard hard hard
+	hard
+	hard hard
+	hard hard hard hard
+
+	hard hard
+
+	hard hard
+
+	hard
+	`)
+}
